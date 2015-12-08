89982fba503b2d6f61f1d5e04f58785438dd2921
akikotoritani <ap.toritani@gmail.com>
2015-12-08 09:10:27 +0000

    correction

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,9 @@
 class MessagesController < ApplicationController
   def index
-<<<<<<< HEAD
     # Messageを全て取得する。
     @messages = Message.all
   end
   
-=======
-       # Messageを全て取得する。
-    @messages = Message.all
-  end
->>>>>>> 6bc159fff5502166888e47608593d1db968b8617
   def create
     @message = Message.new(message_params)
     @message.save
@@ -20,8 +14,5 @@ class MessagesController < ApplicationController
   def message_params
     params.require(:message).permit(:name, :body)
   end
-<<<<<<< HEAD
   
-=======
->>>>>>> 6bc159fff5502166888e47608593d1db968b8617
 end
